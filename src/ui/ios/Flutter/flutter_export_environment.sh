2136e3db16f4c72f9f4a775a2f445fb2b7c3e513
c4ptur3-th3-fl4g <22010222@st.phenikaa-uni.edu.vn>
2024-09-23 21:06:56 +0700

    Update Gradle version to 8.9

--- src/ui/ios/Flutter/flutter_export_environment.sh
+++ src/ui/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\HIEU\Downloads\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\HIEU\Documents\GitHub\app_theo_doi_thoi_quen\src\ui"
+export "FLUTTER_ROOT=/home/bombsquad/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/bombsquad/app_theo_doi_thoi_quen/src/ui"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
--- src/ui/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ src/ui/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\HIEU\Downloads\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\HIEU\Documents\GitHub\app_theo_doi_thoi_quen\src\ui"
+export "FLUTTER_ROOT=/home/bombsquad/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/bombsquad/app_theo_doi_thoi_quen/src/ui"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
