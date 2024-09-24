fe83c2c1e9f1ffb9a6056e1490107f5e27275eb4
c4ptur3-th3-fl4g <22010222@st.phenikaa-uni.edu.vn>
2024-09-24 07:17:57 +0700

    Add .vscode/settings.json and update local.properties

---
+++ src/ui/build/2afd035ff0fa6d8dac87c3a5432c89cd/gen_localizations.d
@@ -0,0 +1,1 @@
+ /home/bombsquad/app_theo_doi_thoi_quen/src/ui/.dart_tool/flutter_gen/gen_l10n/app_localizations_en.dart /home/bombsquad/app_theo_doi_thoi_quen/src/ui/.dart_tool/flutter_gen/gen_l10n/app_localizations.dart:  /home/bombsquad/app_theo_doi_thoi_quen/src/ui/l10n.yaml /home/bombsquad/app_theo_doi_thoi_quen/src/ui/lib/src/localization/app_en.arb
